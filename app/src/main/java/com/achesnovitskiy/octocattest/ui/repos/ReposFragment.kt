00adddfc44e2df46356d642d881c924ba4db6f12
Andrey Chesnovitskiy <a.chesnovitskiy@gmail.com>
2020-07-06 15:34:50 +0300

    [ADDED] progress bar graphic, ReposState and some related methods

--- app/src/main/java/com/achesnovitskiy/octocattest/ui/repos/ReposFragment.kt
+++ app/src/main/java/com/achesnovitskiy/octocattest/ui/repos/ReposFragment.kt
@@ -1,10 +1,14 @@
 package com.achesnovitskiy.octocattest.ui.repos
 
+import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -13,12 +17,15 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.achesnovitskiy.octocattest.R
 import com.achesnovitskiy.octocattest.data.Repo
+import com.achesnovitskiy.octocattest.viewmodels.repos.ReposState
 import com.achesnovitskiy.octocattest.viewmodels.repos.ReposViewModel
 import kotlinx.android.synthetic.main.fragment_repos.*
+
 
 class ReposFragment : Fragment(R.layout.fragment_repos) {
     private lateinit var reposViewModel: ReposViewModel
     private lateinit var reposAdapter: ReposAdapter
+    private val binding: ReposBinding by lazy { ReposBinding() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,28 +36,51 @@ class ReposFragment : Fragment(R.layout.fragment_repos) {
         super.onActivityCreated(savedInstanceState)
 
         setupToolbar()
+        setupProgressBar()
         setupViewModel()
         setupRecyclerView()
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.menu_search, menu)
+
         val searchItem = menu.findItem(R.id.action_search)
         val searchView = searchItem?.actionView as SearchView
-        searchView.apply {
-            queryHint = getString(R.string.search)
 
-            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-                override fun onQueryTextSubmit(query: String?): Boolean {
-//                viewModel.handleSearchQuery(query)
-                    return true
-                }
+        searchView.queryHint = getString(R.string.search)
 
-                override fun onQueryTextChange(newText: String?): Boolean {
-//                viewModel.handleSearchQuery(newText)
-                    return true
-                }
-            })
+        if (binding.isSearch) {
+            searchItem.expandActionView()
+            searchView.setQuery(binding.searchQuery, false)
+        }
+
+        searchItem.setOnActionExpandListener(object : MenuItem.OnActionExpandListener {
+            override fun onMenuItemActionExpand(item: MenuItem?): Boolean {
+                reposViewModel.handleSearchMode(true)
+                return true
+            }
+
+            override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
+                reposViewModel.handleSearchMode(false)
+                return true
+            }
+        })
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                reposViewModel.handleSearchQuery(query)
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                reposViewModel.handleSearchQuery(newText)
+                return true
+            }
+        })
+
+        searchView.setOnCloseListener {
+            reposViewModel.handleSearchMode(false)
+            true
         }
 
         super.onCreateOptionsMenu(menu, inflater)
@@ -60,9 +90,31 @@ class ReposFragment : Fragment(R.layout.fragment_repos) {
         (activity as AppCompatActivity).setSupportActionBar(repos_toolbar)
     }
 
+    private fun setupProgressBar() {
+        // Set progress bar color for API < 21
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            val drawableProgress =
+                DrawableCompat.wrap(repos_progress_bar.indeterminateDrawable)
+
+            DrawableCompat.setTint(
+                drawableProgress,
+                ContextCompat.getColor((activity as AppCompatActivity), R.color.color_accent)
+            )
+
+            repos_progress_bar.indeterminateDrawable = DrawableCompat.unwrap(drawableProgress)
+        }
+    }
+
     private fun setupViewModel() {
         reposViewModel = ViewModelProvider(this).get(ReposViewModel::class.java)
             .apply {
+                getState().observe(
+                    viewLifecycleOwner,
+                    Observer { state ->
+                        binding.bind(state)
+                    }
+                )
+
                 loadReposFromApi(USER_OCTOCAT)
 
                 getRepos().observe(
@@ -98,5 +150,17 @@ class ReposFragment : Fragment(R.layout.fragment_repos) {
 
     companion object {
         const val USER_OCTOCAT = "octocat"
+    }
+
+    inner class ReposBinding {
+        var searchQuery: String? = null
+        var isSearch: Boolean = false
+        var isLoading: Boolean = false
+
+        fun bind(state: ReposState) {
+            isSearch = state.isSearch
+            searchQuery = state.searchQuery
+            isLoading = state.isLoading
+        }
     }
 }
--- app/src/main/java/com/achesnovitskiy/octocattest/viewmodels/repos/ReposViewModel.kt
+++ app/src/main/java/com/achesnovitskiy/octocattest/viewmodels/repos/ReposViewModel.kt
@@ -1,14 +1,36 @@
 package com.achesnovitskiy.octocattest.viewmodels.repos
 
+import android.util.Log
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.achesnovitskiy.octocattest.data.Repo
 
 class ReposViewModel : ViewModel() {
+    private val state = MutableLiveData<ReposState>().apply {
+        value = ReposState()
+    }
     private val repos = MutableLiveData<List<Repo>>()
 
-    fun getRepos(): LiveData<List<Repo>> = repos
+    fun getState(): LiveData<ReposState> = state
+
+    fun getRepos(): LiveData<List<Repo>> {
+        val result = MediatorLiveData<List<Repo>>()
+
+        val filterF = {
+            val query = state.value?.searchQuery ?: ""
+            val repos = repos.value!!
+
+            result.value = if (query.isEmpty()) repos
+            else repos.filter { it.name.contains(query, true) }
+        }
+
+        result.addSource(repos) { filterF.invoke() }
+        result.addSource(state) { filterF.invoke() }
+
+        return result
+    }
 
     fun loadReposFromApi(user: String) {
         repos.value = listOf(
@@ -19,4 +41,25 @@ class ReposViewModel : ViewModel() {
             Repo(2, "Repo 5 $user")
         )
     }
+
+    fun handleSearchQuery(query: String?) {
+        query ?: return
+        updateState { it.copy(searchQuery = query) }
+    }
+
+    fun handleSearchMode(isSearch: Boolean) {
+        updateState { it.copy(isSearch = isSearch) }
+    }
+
+    private fun updateState(update: (currentState: ReposState) -> ReposState) {
+        val updatedState = update(state.value!!)
+        state.value = updatedState
+        Log.d("My_ReposViewModel", "${state.value}")
+    }
 }
+
+data class ReposState(
+    val isSearch: Boolean = false,
+    val searchQuery: String? = null,
+    val isLoading: Boolean = true
+)
